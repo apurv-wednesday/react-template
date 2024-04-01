f2c71dc020a34409e4448f99eb95c5cc512b9210
apurv-wednesday <apurv@wednesday.is>
2024-04-01 22:54:26 +0530

    add storybook to TrackComponent

--- app/components/TrackComponent/stories/TrackComponent.stories.js
+++ app/components/TrackComponent/stories/TrackComponent.stories.js
@@ -7,7 +7,23 @@
  */
 
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { TrackComponent } from '../index';
 
-storiesOf('TrackComponent').add('simple', () => <TrackComponent />);
+export default {
+  title: 'TrackComponent',
+  component: TrackComponent
+};
+
+const Template = (args) => <TrackComponent {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+  trackId: 1, 
+  trackName: "Sample Track",
+  artistName: "Sample Artist", 
+  previewUrl: "https://example.com/sample-preview.mp3", 
+  artworkUrl100: "https://example.com/sample-artwork.jpg",
+  collectionName: "Sample Collection", 
+  pauseTrack: () => console.log("Pause Track Function"), 
+};
